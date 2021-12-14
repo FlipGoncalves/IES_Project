39619e88d5145be87d18b5ce8b257c43d3503f55
FlipGoncalves <filipeg@ua.pt>
2021-12-14 17:36:10 +0000

    templates + controller better

--- demo/src/main/java/com/example/demo/ViewController.java
+++ demo/src/main/java/com/example/demo/ViewController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
@@ -38,8 +37,7 @@ public class ViewController {
 	}
 
 	@GetMapping("/home")
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	public String home(Model model) {
 		return "home";
 	}
 
