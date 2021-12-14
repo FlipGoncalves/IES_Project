d5d8c21dbc121fff11275e38436e422b7096c2f4
Vicente <vicente.costa@ua.pt>
2021-12-14 10:10:01 +0000

    entity

--- TrendIt/src/main/java/Project/app/ProjetoApplication.java
+++ TrendIt/src/main/java/Project/app/ProjetoApplication.java
@@ -3,6 +3,11 @@ package Project.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// useful site : https://twitter4j.org/en/code-examples.html
+
+
+
 @SpringBootApplication
 public class ProjetoApplication {
 
---
+++ TrendIt/src/main/java/Project/app/repository/TweetRepository.java
@@ -0,0 +1,10 @@
+package Project.app.repository;
+import Project.app.model.Tweet;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TweetRepository extends JpaRepository<Tweet,Long> {
+
+    @Query(value = "SELECT * FROM Tweet t where t.tag = ?1 ", nativeQuery=true)
+    Movie findByTag(String tag);
+}
