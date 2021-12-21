88fc658e7ee44fc4bc7fd2ba9ef4fd8d959d7916
Pedro <pdfl@ua.pt>
2021-12-21 15:37:13 +0000

    Random commit

--- Project/src/main/java/TwitterBot/model/SearchTweets/Datum.java
+++ Project/src/main/java/TwitterBot/model/SearchTweets/Datum.java
@@ -12,58 +12,59 @@ public class Datum {
   String id;
   String lang;
   String text;
-  
+
   @SerializedName("author_id")
   public String getAuthor_id() {
     return this.author_id;
   }
-  
+
   public void setAuthor_id( String author_id ) {
     this.author_id = author_id;
   }
-  
+
   @SerializedName("conversation_id")
   public String getConversation_id() {
     return this.conversation_id;
   }
-  
+
   public void setConversation_id( String conversation_id ) {
     this.conversation_id = conversation_id;
   }
-  
+
   @SerializedName("created_at")
   public Date getCreated_at() {
     return this.created_at;
   }
-  
+
   public void setCreated_at( Date created_at ) {
     this.created_at = created_at;
   }
-  
+
   @SerializedName("id")
   public String getId() {
     return this.id;
   }
-  
+
   public void setId( String id ) {
     this.id = id;
   }
-  
+
   @SerializedName("lang")
   public String getLang() {
     return this.lang;
   }
-  
+
   public void setLang( String lang ) {
     this.lang = lang;
   }
-  
+
   @SerializedName("text")
   public String getText() {
     return this.text;
   }
-  
+
   public void setText( String text ) {
     this.text = text;
   }
+
 }
