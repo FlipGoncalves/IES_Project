9ec5d8a6ec2164964cc213e7c7403d226aa4ce73
Pedro <pdfl@ua.pt>
2021-12-21 14:20:39 +0000

    Added latest service

--- Project/src/main/java/TwitterBot/Services/TTService.java
+++ Project/src/main/java/TwitterBot/Services/TTService.java
@@ -1,18 +1,18 @@
 package TwitterBot.Services;
 
+import TwitterBot.model.Datum;
 import TwitterBot.model.TweetCount;
 import TwitterBot.model.TweetTrendsJson;
-import TwitterBot.model.Tweets;
 
 import java.util.List;
 
-public interface Service {
+public interface TTService {
   
   
   List<TweetTrendsJson> getTrends( long id );
   
   List<TweetCount> getInterestCount( String query );
   
-  List<Tweets> searchTweets( String query);
+  List<Datum> searchTweets( String query);
   
 }
